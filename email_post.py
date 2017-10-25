ccc41daf47fb8fcd6a18bb88b18a765605618034
susecjh <susecjh@gmail.com>
2017-10-25 23:52:44 +0800

    qq email is completed

--- email_post.py
+++ email_post.py
@@ -6,33 +6,32 @@ from re import match
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
+from email.utils import parseaddr,formataddr
 
-"""封装qq邮箱，gmail邮箱，163邮箱的发送"""
-
-
+#封装qq邮箱，gmail邮箱，163邮箱的发送
 class EmailPost(object):
-    def __init__(self, type, from_addr, password):
+    def __init__(self, tp, from_addr, password):
         """
         :param type: 你发送邮箱的方式，可选内容为:qq,QQ,gmail,163
         :param from_addr: 你的邮箱账号
         :param password: 你的邮箱密码，其中qq方式的发送password为授权码
         """
-        if not isinstance(type, str):
+        if not isinstance(tp, str):
             raise ValueError('type is not a str, please input "qq","QQ","gmail","163"!')
-        if type == 'qq' or type == 'QQ':
-            if match('^\w+@qq.com$', from_addr) is None:
+        if tp == 'qq' or tp == 'QQ':
+            if match(r'^\w+@qq.com$', from_addr) is None:
                 raise ValueError('The email, %s , is not a %s email' % (from_addr, type))
-        if type == 'gmail':
-            if match('^\w+@gmail.com$', from_addr) is None:
+        if tp == 'gmail':
+            if match(r'^\w+@gmail.com$', from_addr) is None:
                 raise ValueError('The email, %s , is not a %s email' % (from_addr, type))
-        if type == '163':
-            if match('^\w+@163.cn$', from_addr) is None:
+        if tp == '163':
+            if match(r'^\w+@163.cn$', from_addr) is None:
                 raise ValueError('The email, %s , is not a %s email' % (from_addr, type))
-        self.__type = type
+        self.__type = tp
         self.__from_addr = from_addr
         self.__password = password
-        self.__server = self.open()
-    
+        self.open()
+        
     def open(self):
         """
         :param type: 邮箱类型
@@ -44,6 +43,7 @@ class EmailPost(object):
             self.__server = self.open_gmail()
         if self.__type == '163':
             self.__server = self.open_163()
+
         self.__server.login(self.__from_addr, self.__password)
     
     def open_qq(self):
@@ -60,7 +60,8 @@ class EmailPost(object):
             msg:你想要发送的内容，可以是html的方式
             subject----主题
             kwargs:
-                subtype:发送内容的格式----   'plain'---发送的文本内容，是默认值
+                subtype:发送内容的格式----   
+                                           'plain'---发送的文本内容，是默认值
                                            'html'----发送html方式
                                            'all'----用时支持以上两种方式
                 html_msg----'all' 存在时生效，存储html方式的内容
@@ -75,7 +76,8 @@ class EmailPost(object):
         message['From'] = _format_addr('%s<%s>'%(self.__from_addr,kwargs.get('from_addr','susecjh')))
         message['To'] = _format_addr('%s<%s>'%(to_addr,kwargs.get('to_addr','用户')))
         message['Subject'] = Header(subject,'utf-8').encode()
-        self.__server.sendmail(from_addr=self.__from_addr, to_addrs=to_addrs, msg=message.as_string())
+        self.__server.set_debuglevel(1)
+        self.__server.sendmail(from_addr=self.__from_addr, to_addrs=to_addr, msg=message.as_string())
 
     def __del__(self):
         self.__server.quit()
@@ -83,3 +85,8 @@ class EmailPost(object):
 def _format_addr(s):
 	name,addr = parseaddr(s)
 	return formataddr((Header(name,'utf-8').encode(),addr))
+
+if __name__ == '__main__':
+    emailPost = EmailPost('qq','571639000@qq.com','hlzmwrvufyrrbcbf')
+    emailPost.post('susecjh@gmail.com','哈哈','emmmmm')
+    del emailPost
